a67db6c9dbb3581802d33b37561333cce0f6245e
Costas Tyfoxylos <costas.tyf@gmail.com>
2017-05-12 11:57:29 +0200

    added documentation

--- mijnahlib/mijnahlib.py
+++ mijnahlib/mijnahlib.py
@@ -26,6 +26,11 @@ LOGIN_ERROR_MESSAGE = 'Het e-mailadres en/of wachtwoord is onjuist'
 
 
 class Server(object):
+    """Object modeling the server connection.
+    
+    Handles the authentication and exposes all the internal parts as 
+    attributes.
+    """
     def __init__(self, username, password):
         logger_name = '{base}.{suffix}'.format(base=LOGGER_BASENAME,
                                                suffix=self.__class__.__name__)
@@ -61,6 +66,10 @@ class Server(object):
 
 
 class ShoppingCart(object):
+    """Object modeling the shopping cart.
+    
+    It is able to add items to the cart, by id or description.
+    It exposes item objects through the content attribute"""
     def __init__(self, ah_instance):
         logger_name = '{base}.{suffix}'.format(base=LOGGER_BASENAME,
                                                suffix=self.__class__.__name__)
@@ -70,9 +79,21 @@ class ShoppingCart(object):
                      'items').format(base=self._ah.url)
 
     def add_item_by_id(self, item_id, quantity=1):
+        """Adds items to the shopping cart by the internal id representation
+
+        :param item_id: The internal representation of the item
+        :param quantity: The quantity as an integer
+        :return: True on success False otherwise.
+        """
         return self._add_item('PRODUCT', 'id', item_id, quantity)
 
     def add_item_by_description(self, description, quantity=1):
+        """Adds items to the shopping cart by the internal id representation
+
+        :param description: The description of the item
+        :param quantity: The quantity as an integer
+        :return: True on success False otherwise.
+        """
         return self._add_item('UNSPECIFIED',
                               'description',
                               description,
@@ -87,6 +108,10 @@ class ShoppingCart(object):
 
     @property
     def contents(self):
+        """The contents of the shopping cart
+
+        :return: A list of items according to their type
+        """
         url = ('{base}/service/rest/delegate'
                '?url=%2Fmijnlijst').format(base=self._ah.url)
         response = self._ah.session.get(url)
@@ -100,10 +125,15 @@ class ShoppingCart(object):
         return products
 
     def get_items_with_discount(self):
+        """Get the items that are on discount on the shopping cart
+
+        :return: A list of product objects or empty.
+        """
         return [item for item in self.contents if item.has_discount]
 
 
 class ItemFactory(object):
+    """A factory that instantiates the appropriate object based on type"""
     def __new__(cls, ah_instance, info):
         product_type = info.get('type').lower()
         if product_type == 'product':
@@ -113,6 +143,10 @@ class ItemFactory(object):
 
 
 class Item(object):
+    """Generic class of the products objects
+    
+    Handles all the common parts
+    """
     def __init__(self, ah_instance, info):
         logger_name = '{base}.{suffix}'.format(base=LOGGER_BASENAME,
                                                suffix=self.__class__.__name__)
@@ -122,16 +156,22 @@ class Item(object):
 
     @property
     def url(self):
+        """The url of the item
+
+        :return: The url of the item
+        """
         _url = self._info.get('navItem', {}).get('link', {}).get('href', {})
         return self._ah.url + _url
 
 
 class UnspecifiedProduct(Item):
+    """An object to model the unspecified products"""
     def __init__(self, ah_instance, info):
         super(UnspecifiedProduct, self).__init__(ah_instance, info)
 
     @property
     def description(self):
+        """The description of the product"""
         description = self._info.get('description')
         return description.replace(u'\xad', u'')
 
@@ -146,6 +186,7 @@ class UnspecifiedProduct(Item):
 
 
 class Product(Item):
+    """An object to model the products."""
     def __init__(self, ah_instance, info):
         super(Product, self).__init__(ah_instance, info)
 
@@ -155,39 +196,50 @@ class Product(Item):
 
     @property
     def id(self):
+        """The internal id of the item"""
         return self._product.get('id')
 
     @property
     def is_orderable(self):
-        return self._product.get('availability', {}
-                                ).get('orderable', False)
+        """Whether the object is orderable on not
+
+        :return: A boolean of the state
+        """
+        return self._product.get('availability', {}).get('orderable', False)
 
     @property
     def category(self):
+        """The category of the product"""
         return self._product.get('categoryName')
 
     @property
     def price(self):
+        """The price of the product"""
         return self._product.get('priceLabel', {}).get('now')
 
     @property
     def price_previously(self):
+        """The previous price of the product if on discount."""
         return self._product.get('priceLabel', {}).get('was')
 
     @property
     def has_discount(self):
+        """Whether the object is a discounted one"""
         discount = self._product.get('discount')
         return True if discount else False
 
     @property
     def measurement_unit(self):
+        """The measurement unit of the product according to AH"""
         return self._product.get('unitSize')
 
     @property
     def description(self):
+        """The description of the object"""
         description = self._product.get('description')
         return description.replace(u'\xad', u'')
 
     @property
     def brand(self):
+        """The brand of the product"""
         return self._product.get('brandName')
